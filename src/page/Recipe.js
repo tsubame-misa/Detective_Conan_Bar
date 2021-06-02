5b722dee933c17800e36bbe80412903aef14b70b
tsubame_misa <chmi19013@g.nihon-u.ac.jp>
2021-06-02 18:05:02 +0900

    add loading

--- src/page/Recipe.js
+++ src/page/Recipe.js
@@ -6,13 +6,22 @@ import { fetchRecipe } from "../api";
 
 function Recipe() {
   const { name } = useParams();
-  const [recipe, setRecipe] = useState([]);
+  const [recipe, setRecipe] = useState();
   useEffect(() => {
     fetchRecipe(name).then((data) => {
       setRecipe(data.cocktails);
     });
   }, []);
+  console.log(recipe);
 
+  if (recipe === undefined) {
+    return (
+      <div style={{ height: "90vh", padding: "20vh" }}>
+        <div className="pageloader"></div>
+        <h1 className="has-text-white has-text-centered">loading...</h1>
+      </div>
+    );
+  }
   return (
     <div className="has-text-white">
       {name === "キール" ? (
